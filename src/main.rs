0ede32644500bcbbd653b751865e81988bc0b710
Myra Roberts <myra@myrari.net>
2025-12-24 15:16:17 -0700

    basic eframe setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+struct App {}
+
+impl App {
+    pub fn new(_: &eframe::CreationContext<'_>) -> Self {
+        Default::default()
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        App {}
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // theme widget
+            egui::widgets::global_theme_preference_buttons(ui);
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello from inscribe_rs");
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let _ = eframe::run_native(
+        "inscribers",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder {
+                title: Some("wip-inscribers".into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    );
 }
